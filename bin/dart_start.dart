69abd6fc4b539de47aa9bf237bfa25de98352a1b
kis <pdg300@ccmedia.co.kr>
2024-07-17 18:03:16 +0900

    24.07.17 dart 기본문법 학습 -kis

---
+++ bin/dart_start.dart
@@ -0,0 +1,26 @@
+import 'package:dart_start/dart_start.dart' as dart_start;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_start.calculate()}!');
+
+  int age = 25;
+
+  String name = "Kim Doe";
+
+  bool flag = false;
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  Map<String, int> scores = {
+    'Alice': 10,
+    'Bob': 20,
+    'Charlie': 30,
+  };
+
+  print(age);
+  print(name);
+  print(flag);
+  print(numbers);
+  print(scores);
+
+}
---
+++ lib/quest_1.dart
@@ -0,0 +1,8 @@
+
+String favoriteColor(String color) {
+  return "My favorite color is $color";
+}
+
+List<int> initGrades() {
+  return [10, 20, 30, 40, 50];
+}
